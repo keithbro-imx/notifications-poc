9bdb8fe35d3404a0cfa141971071d6c04bf27698
Keith Broughton <keith.broughton@immutable.com>
2024-07-23 20:49:36 +1000

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,35 @@
-'use client';
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
+  const [permission, setPermission] = useState("");
   const click = async () => {
     const res = await Notification.requestPermission();
-    console.log({res})
+    console.log({ res });
+    setPermission(res);
 
-    setInterval(() => {
-      console.log("sending")
-      new Notification('Hello', {});
-      console.log("sent")
+    const x = setInterval(() => {
+      console.log("sending");
+      const notif = new Notification("Hello " + Math.random(), {
+        body: "Hello, World!" + Math.random(),
+      });
+      notif.addEventListener("show", (event) => {
+        console.log("Notification shown", event);
+      });
+      console.log({ notif });
     }, 2000);
-
-  }
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={click} className="bg-blue-500 text-white px-4 py-2 rounded">Click Me</button>
+      <div> Permission: {permission}</div>
+      <button
+        onClick={click}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Click Me
+      </button>
     </main>
   );
 }
